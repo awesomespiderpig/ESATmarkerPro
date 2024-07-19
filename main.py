7817843318f071653a98cbdcf81719ec987b34f6
Edward Press <presseddie@hotmail.com>
2024-07-19 13:51:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 answers = []
 inputs = []
+mistakes = []
 score = 0
 
 with open('answers.txt', 'r') as file:
@@ -13,7 +14,12 @@ with open('inputs.txt', 'r') as file:
 for i in range(len(answers)):
     if inputs[i] == answers[i]:
         score += 1
+    else:
+        mistake = f"Question {(i+1)}: {answers[i]} not {inputs[i]}"
+        mistakes.append(mistake)
 
 percentage = (score/len(answers)*100).__round__(1)
 print(f"Score was: {score}/{len(answers)}")
-print(f"Percentage: {percentage}")
+print(f"Percentage: {percentage}%")
+for i in mistakes:
+    print(i)
